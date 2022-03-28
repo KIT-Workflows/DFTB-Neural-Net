9ac1c71204fbf6ce3931d9fefe3cb07bbf063ffd
Orgup <51993842+tipiorgup@users.noreply.github.com>
2022-03-28 14:12:28 +0200

    Cleaning

--- src/Calculator/installCython.sh
+++
@@ -1,4 +0,0 @@
-./uninstallCython.sh
-mkdir build
-python3 setup.py build_ext --inplace
-#mv ./SymmDerivIndCython.cpython-36m-x86_64-linux-gnu.so ./SymmFuncIndCython.cpython-36m-x86_64-linux-gnu.so ..
--- src/Tools/switch_th.sh
+++
@@ -1,4 +0,0 @@
-# Script to Switch Keras Backend To Theano
-source deactivate 
-cp ~/.keras/keras_th.json ~/.keras/keras.json
-source activate theano
